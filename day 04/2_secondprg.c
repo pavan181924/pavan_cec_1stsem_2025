1f255f1d6638e94ac1624ec807d6f911ca77b613
prajwal <prajwal>
2025-11-20 01:45:50 -0800

    day o4 code pushed

---
+++ day 04/2_secondprg.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("enter the values");
+    scanf("%d%d%d", &a, &b, &c);
+    int sum = a + b + c;
+    printf("sum of the numbers=%d", sum);
+    return 0;
+}
---
+++ day 04/3_marks.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    float marks=550;
+    printf("your marks is:%f",marks);
+    int age=19;
+    printf("\n your age is %d",age);
+    double pi=18.1924;
+    printf("\n \?value of pi:%lf",pi);
+    char grade = 'p';
+    printf("\n grade\t=%c",grade);
+    bool isdead=false;
+    printf("\n is\"dead\":%d",isdead);
+
+
+ 
+}
